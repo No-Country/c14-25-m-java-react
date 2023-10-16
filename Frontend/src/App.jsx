246abef91d4033950921eb1e92b05d60e1667522
GuidoRM <guidorafaelmamani100@gmail.com>
2023-10-16 17:31:02 -0300

    feat: Setup fonts and SvgIcons

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import SvgError from "./assets/icons/SvgError"
+
 function App() {
   
   return (
     <>
-      <h1>Presupuesto Claro</h1>
+      <h1>Presupuesto Claro <SvgError/></h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum eligendi ea nulla? In voluptatum aliquam, numquam enim velit quam accusamus veritatis eos dicta deleniti odit ab nulla molestiae adipisci?</p>
     </>
   )
-  
+
 }
 
 export default App
