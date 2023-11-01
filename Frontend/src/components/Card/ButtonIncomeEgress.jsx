6241a6fa4a2df2d2e0f000659d578123a6653b5c
Valentino <valentinoindorato@gmail.com>
2023-11-01 20:41:41 -0300

    correccion de funcion en buttonIncomEgress

--- Frontend/src/components/Card/ButtonIncomeEgress.jsx
+++ Frontend/src/components/Card/ButtonIncomeEgress.jsx
@@ -4,7 +4,7 @@ function ButtonIncomeEgress({ amount,changeClass, income, egress, formTrue, rese
         <div className="tabs">
             <button
               onClick={() => {
-                income();reset() 
+                reset(); income()
               }}
               className={`${changeClass ? "blue" : ""} ${formTrue ? "formDashboard-modalTabs" : ""} `}
          
@@ -14,7 +14,7 @@ function ButtonIncomeEgress({ amount,changeClass, income, egress, formTrue, rese
             </button>
             <button
               onClick={() => {
-                egress();reset() 
+                reset() ;egress()
               } }
               className={`${!changeClass ? "red": ""} ${formTrue ? "formDashboard-modalTabs" : ""}  `}
             >
